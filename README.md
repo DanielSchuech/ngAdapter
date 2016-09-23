ea41eb76eb2fd8ec826cde7c41c9f0df32ecff4f
Daniel Schuech <dschuech@gmail.com>
2016-09-23 09:53:42 +0200

    docs(*): readme update, docu ngAdapter function
    add description and examples to each exported function

--- README.md
+++ README.md
@@ -26,10 +26,23 @@ adapter.bootstrap(document.body, ['MyApp']);
 
 ## Downgrade Functions
 ```javascript
+// downgrade Component & Directive
 module.directive('myNg2Component', adapter.downgradeNg2Component(MyNg2Component));
 module.directive('myNg2Directive', adapter.downgradeNg2Directive(MyNg2Directive));
 
-adapter.addProvider(MyNg2Service);
+//downgrade NgModule
+@NgModule({})
+class MyNg2Module {}
+adapter.addNg2Module(MyNg2Module);
+let moduleName = adapter.downgradeNg2Module(MyNg2Module); //'MyNg2Module'
+angular.module('App').requires.push(moduleName);
+
+//downgrade Service
+@NgModule({
+  providers: [MyNg2Service]
+})
+class MyNg2Module {}
+adapter.addNg2Module(MyNg2Module);
 module.factory('myNg2Service', adapter.downgradeNg2Provider(MyNg2Service));
 ```
 
@@ -41,16 +54,19 @@ upgradeAdapter.upgradeNg1Provider('myNg1Service');
     selector: 'my-ng2-component',
     template: `
       <span my-ng1-attribute-directive>Hello World</span>
-      <my-ng1-directive></my-ng1-directive>>
-    `,
-    directives: [
-      adapter.upgradeNg1Directive('myNg1AttributeDirective'),
-      adapter.upgradeNg1Directive('myNg1Directive')
-    ]
+      <my-ng1-directive></my-ng1-directive>
+    `
 })
 export class AppComponent {
   constructor(@Inject('myNg1Service') service: MyNg1Service) {}
 }
-```
 
-## 
+@NgModule({
+  directives: [
+    adapter.upgradeNg1Directive('myNg1AttributeDirective'),
+    adapter.upgradeNg1Component('myNg1Directive'),
+    AppComponent
+  ]
+})
+class MyNg1Module {}
+```
--- src/ngAdapter.ts
+++ src/ngAdapter.ts
@@ -40,27 +40,95 @@ export class ngAdapter {
     this.ng2Modules.push(ngAdapterModul);
   }
   
+  /**
+   * bootstrap the AngularJS and Angular 2 hybrid appication
+   */
   bootstrap(element: Element, app: string[]) {
     return <any>this.upgradeAdapter.bootstrap(element, app);
   }
   
+  /**
+   * downgrade an Angular 2 component to AngularJS
+   * 
+   * @example
+   * angular.module('app').directive('MyNg2Component', adapter.downgradeNg2Component(MyNg2Component));
+   */
   downgradeNg2Component(cmp: Type<any>): Function {
     return <any>this.upgradeAdapter.downgradeNg2Component(cmp);
   }
   
+  /**
+   * downgrade an Angular 2 directive to AngularJS
+   * 
+   * @example
+   * angular.module('app').directive('myHighlight', adapter.downgradeNg2Directive(MyHighlight));
+   */
   downgradeNg2Directive(directive: any): Function {
     return this.downgrade.directive(directive);
   }
   
+  /**
+   * downgrade an Angular 2 Service to AngularJS
+   * the services has to be added to Angular 2 before
+   * 
+   * @example
+   * @Injectable()
+   * class MyService {}
+   * 
+   * @NgModule({
+   *  providers: [MyService]
+   * })
+   * class MyModule {}
+   * 
+   * adapter.addNg2Module(MyModule);
+   * 
+   * angular.module('app').factory('myService', adapter.downgradeNg2Directive(MyService));
+   */
   downgradeNg2Provider(provider: any): Function {
     return <any>this.upgradeAdapter.downgradeNg2Provider(provider);
   }
   
+  /**
+   * upgrade an AngularJS element directive to Angular 2
+   * WARNING: You ALWAYS have to downgrade your Angular 2 Component at the end!
+   * 
+   * @example
+   * @Component({
+   *  selector: 'app-component',
+   *  template: '<my-ng1-element></my-ng1-element>'
+   * })
+   * class AppComponent {}
+   * 
+   * @NgModule({
+   *  declarations: [adapter.upgradeNg1Component('myNg1Element')]
+   * })
+   * 
+   * angular.module('app').directive('MyNg2Component', adapter.downgradeNg2Component(MyNg2Component));
+   */
   upgradeNg1Component(directive: string): Type<any> {
     this.upgradedCmpAndDir.push(directive);
     return this.upgradeAdapter.upgradeNg1Component(directive);
   }
   
+  /**
+   * upgrade an AngularJS element directive to Angular 2
+   * WARNING: You ALWAYS have to downgrade your Angular 2 Component at the end!
+   * 
+   * scope bindings of the AngularJS Directive has to be used in Angular 2 with Angular 2 Syntax 
+   * 
+   * @example
+   * @Component({
+   *  selector: 'app-component',
+   *  template: '<div my-ng1-highlight [myVar]="'myString'">Hello World</div>'
+   * })
+   * class AppComponent {}
+   * 
+   * @NgModule({
+   *  declarations: [adapter.upgradeNg1Directive('myNg1Highlight')]
+   * })
+   * 
+   * angular.module('app').directive('MyNg2Component', adapter.downgradeNg2Component(MyNg2Component));
+   */
   upgradeNg1Directive(directive: string): Type<any> {
     //let upgrade = new Upgrade(this.upgradeAdapter, this.module, this.addedProviders,
     //  this.upgradedProviders);
@@ -68,11 +136,27 @@ export class ngAdapter {
     return this.upgrade.upgradeNg1Directive(directive);
   }
   
+  /**
+   * upgrade an AngularJS Service and inject it to the Angular 2 main module
+   * 
+   * @example
+   * angular.module('app').service('myNg1Service', MyNg1Service);
+   * adapter.upgradeNg1Provider('myNg1Service');
+   */
   upgradeNg1Provider(provider: string) {
     this.upgradedProviders.push(provider);
     return this.upgradeAdapter.upgradeNg1Provider(provider);
   }
 
+  /**
+   * add Angular 2 Module to the Angular 2 main module
+   * 
+   * @example
+   * @NgModule({})
+   * class MyModule {}
+   * 
+   * adapter.addNg2Module(MyModule);
+   */
   addNg2Module(mod: Type<any>) {
     this.ng2Modules.push(mod);
 
@@ -81,6 +165,25 @@ export class ngAdapter {
     this.addedProviders.push(...meta.providers);
   }
 
+  /**
+   * downgrade an added Angular 2 Module to an AngularJS Module
+   * an AngularJS Module with the name of the given class/function will be created
+   * returns name of module as string
+   * 
+   * NgModule has to be added before downgrading
+   * 
+   * instead of adding the module name after downgrading to the requries field of an AngularJS Module
+   * you can add it on creating even if it doesn´t exists at this moment
+   * angular.module('app', ['MyNg2Module']);
+   * 
+   * @example
+   * @NgModule({})
+   * class MyNg2Module {}
+   * 
+   * adapter.addNg2Module(MyNg2Module);
+   * let moduleName = adapter.downgradeNg2Module(MyNg2Module); //'MyNg2Module'
+   * angular.module('App').requires.push(moduleName);
+   */
   downgradeNg2Module(mod: Type<any>) {
     if (this.ng2Modules.indexOf(mod) < 0) {
       throw Error('NgModule has to be added with adapter.addNg2Module first!');
